a08fbe4b491afd07e8ddcc29f083714ec68c6da1
Alperen Tosun <alperentosun@yahoo.com>
2019-12-14 16:33:05 +0300

    Add project files.

---
+++ ECommerce/Areas/Management/Models/Entities/SubModel.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Areas.Management.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Areas.Management.Models.Entities
+{
+    public class SubModel:BaseEntity
+    {
+       
+        public int modelId { get; set; }
+      
+        public virtual  Model Model { get; set; }
+        public virtual ICollection<Product> Product { get; set; }
+    }
+}
